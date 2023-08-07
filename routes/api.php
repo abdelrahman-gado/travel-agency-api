a1f59266495bcdf810d49c90030470f17378947c
abdelrahman-gado <abdelrahman.magdy.gado@gmail.com>
2023-08-07 20:17:39 +0300

    add roles routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::apiResource('roles', RoleController::class);
