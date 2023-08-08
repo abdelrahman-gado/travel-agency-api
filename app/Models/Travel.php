ab0950f7b939d9bbceb4dce962b20898c3333b1d
abdelrahman-gado <abdelrahman.magdy.gado@gmail.com>
2023-08-08 20:56:24 +0300

    create travel model

---
+++ app/Models/Travel.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Travel extends Model
+{
+    use HasFactory;
+
+    public $table = 'travels';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'is_public',
+        'slug',
+        'name',
+        'description',
+        'number_of_days'
+    ];
+
+    /**
+     * Get the tours for a travel.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tours(): HasMany
+    {
+        return $this->hasMany(Tour::class);
+    }
+}
